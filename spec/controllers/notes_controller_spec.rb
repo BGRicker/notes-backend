768d90fbc24b6af54d0d5be990a374c3f0e29a50
Ben Ricker <BGRicker@gmail.com>
2017-05-10 21:44:11 -0400

    associating tags

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -16,6 +16,14 @@ RSpec.describe NotesController, type: :controller do
 			json = JSON.parse(response.body)
 			expect(json[0]['id'] < json[1]['id']).to be true
 		end
+
+		it "should include associated tags in response" do
+			note = FactoryGirl.create(:note)
+			tag = FactoryGirl.create(:tag, note_id: note.id)
+			get :index
+			json = JSON.parse(response.body)
+			expect(json[0]['tags'][0]['name']).to eq(tag.name)
+		end
 	end
 
   describe "notes#create action" do
@@ -56,13 +64,23 @@ RSpec.describe NotesController, type: :controller do
   end
 
   describe "notes#show" do
-    it "should show you a note" do
-      note = FactoryGirl.create(:note)
-      get :show, params: { id: note.id }
-      json = JSON.parse(response.body)
-      expect(json['id']).to eq(note.id)
+		before do
+      @note = FactoryGirl.create(:note)
 		end
-	end
+
+    it "should show you a note" do
+      get :show, params: { id: @note.id }
+      json = JSON.parse(response.body)
+      expect(json['id']).to eq(@note.id)
+    end
+
+    it "should include associated tags in response" do
+      tag = FactoryGirl.create(:tag, note_id: @note.id)
+      get :show, params: { id: @note.id }
+      json = JSON.parse(response.body)
+      expect(json['tags'][0]['name']).to eq(tag.name)
+    end
+  end
 
 	describe "notes#update" do
 		before do
